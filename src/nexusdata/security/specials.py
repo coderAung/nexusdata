bc683f83b1a4f235c82d8691fa2abaa6393c8dcf
coderAung <yewontaung798@gmail.com>
2026-01-07 22:05:00 +0630

    add user none check and user supplier invocation to nexus security

--- src/nexusdata/security/specials.py
+++ src/nexusdata/security/specials.py
@@ -2,23 +2,23 @@ import inspect
 from dataclasses import dataclass
 from functools import wraps
 from inspect import Parameter
-from typing import Callable, Any
+from typing import Callable, Any, Generic, Type
 
 from nexusdata.core.metadata.typings import R, U, E
 from nexusdata.utils.exceptions import NexusSecurityException
 
 
 @dataclass(frozen=True)
-class NexusSecurity:
-    user_cls:U
-    role_cls:R
+class NexusSecurity(Generic[U, R, E]):
+    user_cls:Type[U]
+    role_cls:Type[R]
     user_supplier:Callable[[], U]
     role_func:Callable[[U], R]
     exception:E = NexusSecurityException("Forbidden access.")
 
     AUTHENTICATED_USER = "authenticated_user"
 
-    def authenticated(self, func:Callable[..., Any], *, authorized_roles:list[R] = None):
+    def authenticated(self, func:Callable[..., Any] | None = None, *, authorized_roles:list[R] = None):
         def decorator(fn:Callable[..., Any]):
             sig = inspect.signature(fn)
             user_param = Parameter(
@@ -36,9 +36,15 @@ class NexusSecurity:
                 if kwargs.__contains__(self.AUTHENTICATED_USER):
                     user:U = kwargs.get(self.AUTHENTICATED_USER)
                     kwargs.pop(self.AUTHENTICATED_USER)
-                    if authorized_roles and self.role_func(user) not in authorized_roles:
-                        raise self.exception
+                else:
+                    user:U = self.user_supplier()
+                if user is None:
+                    raise NexusSecurityException("Authenticated is not found.")
+
+                if authorized_roles and self.role_func(user) not in authorized_roles:
+                    raise self.exception
                 return fn(*args, **kwargs)
+
             wrapper.__signature__ = new_sig
             return wrapper
 
