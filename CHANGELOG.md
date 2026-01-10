b02d331d312725595140e4f5b7d88c6c30e910e3
coderAung <yewontaung798@gmail.com>
2026-01-10 18:07:44 +0630

    fix naming error

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.2.0] => [0.2.1] - 10.01.2026
+## [0.2.0] => [0.2.2] - 10.01.2026
  ### Import decorators to main package
  ### Set alias for async decorators
--- src/nexusdata/__init__.py
+++ src/nexusdata/__init__.py
@@ -1,5 +1,5 @@
 from nexusdata.legacy.decorators import transactional, query
-from nexusdata.asyncs.decorators import transactional as async_transactional, async_query
+from nexusdata.asyncs.decorators import transactional as async_transactional, query as async_query
 from nexusdata.orms.repositories import NexusRepository, AsyncNexusRepository
 from nexusdata.orms.services import NexusService, AsyncNexusService
 
